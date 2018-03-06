ab0e82f89a62c30dfacf29ef8fd84a2f7ad23890
ClaudeJanssenPro <claudejanssen.pro@gmail.com>
2018-03-06 18:33:41 +0100

    setup01

--- user_profile.php
+++ user_profile.php
@@ -8,10 +8,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>O'cto Chat</title>
+  <title>Profil</title>
 </head>
 <body>
-  <iframe src="conversation.php" name="conversation"></iframe>
-  <iframe src="form_reg" name="form_reg"></iframe>
+
 </body>
 </html>
