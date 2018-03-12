6649e5a9171f49adbd38821ab9a54256caf3ab9d
Claude Janssen <claudejanssen.pro@gmail.com>
2018-03-12 09:18:27 +0100

    monday003

--- index.php
+++ index.php
@@ -3,16 +3,22 @@
 
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/phpchatdb_style.css" charset="utf-8" />
-  <title>O'cto Chat</title>
-</head>
-<body>
-  <iframe src="php/login.php" name="login"></iframe>
-  <iframe src="php/display.php" name="messages"></iframe>
-  <iframe src="php/write.php" name="writer"></iframe>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/phpchatdb_style.css" charset="utf-8" />
+    <title>O'cto Chat</title>
+  </head>
+  <body>
+    <div>
+      <iframe class="login" src="php/login.php" name="login"></iframe>
+    </div>
+    <div>
+      <iframe class="messages" src="php/display.php" name="messages"></iframe>
+    </div>
+    <!-- <div>
+      <iframe class="writer" src="php/write.php" name="writer"></iframe>
+    </div> -->
+  </body>
 </html>
